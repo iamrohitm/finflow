d1d517f02476cc5440f9fd060969b9d2c12fedf2
Rohit Mohite <rohitmohite.in@gmail.com>
2026-06-19 00:03:25 +0530

    doc created

--- src/components/SignupSignin/index.jsx
+++ src/components/SignupSignin/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import './styles.css';
 import Input from '../Input';
-import Button from '../Button';
+import Button from '../Button'; 
 import { toast } from 'react-toastify';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db} from '../../firebase';
+import { doc, setDoc, getDoc } from "firebase/firestore"; 
+import { useNavigate } from 'react-router-dom';
 
 
 const SignupSigninComponent = () => {
@@ -13,6 +15,8 @@ const SignupSigninComponent = () => {
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
   const [loading, setLoading] = useState(false)
+  const [loginForm, setLoginForm] = useState(false)
+  const navigate = useNavigate()
 
   function signupWithEmail() {
     setLoading(true)
@@ -36,6 +40,7 @@ const SignupSigninComponent = () => {
           setPassword("")
           setConfirmPassword("")
           createDoc(user)
+          navigate("/dashboard")
           //Create a doc with user id as the following id
         })
         .catch((error) => {
@@ -56,62 +61,167 @@ const SignupSigninComponent = () => {
 
   }
 
-  function createDoc(user){
+  function loginUsingEmail(){
+    setLoading(true)
+    console.log(email)
+    console.log(password)
+
+    if(email!="" && password!=""){
+
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        toast.success("User Logged In!")
+        console.log("User logged in", user)
+        setLoading(false)
+        navigate("/dashboard")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(errorMessage)
+        setLoading(false)
+      });
+    }else{
+      toast.error("All fields are mandatory!")
+      setLoading(false)
+    }
+  }
+
+  async function createDoc(user){
     //make sure that doc with the uid doesn't exist
     //create a doc
+    setLoading(true)
+    if (!user) return;
+
+    const userRef = doc(db, "users", user.uid);
+    const userData = await getDoc(userRef);
+
+    if (!userData.exists()){
+      try{
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName ? user.displayName : name,
+          email: user.email,
+          photoURL: user.photoURL ? user.photoURL : "",
+          createdAt: new Date(),
+        });
+        toast.success("Doc created!")
+        setLoading(false)
+      }catch(e){
+        toast.error(e.message)
+        setLoading(false)
+      }
+    }else{
+      toast.error("Doc already exists!")
+      setLoading(false)
+    }
   }
 
   return (
-    <div className='signup-wrapper'>
-      <h2 className='title'>
-        Sign Up on <span style={{color: "var(--theme)"}}>FinFlow.</span> 
-      </h2>
+    <>
+      {loginForm ? 
+        <div className='signup-wrapper'>
+          <h2 className='title'>
+            Login on <span style={{color: "var(--theme)"}}>FinFlow.</span> 
+          </h2>
 
-      <form>
-        <Input 
-          type = 'text'
-          label={"Full Name"}
-          state={name}
-          setState={setName}
-          placeholder={"John Doe"} 
-        />
+          <form>
+            <Input 
+              type = 'email'
+              label={"Email"}
+              state={email}
+              setState={setEmail} 
+              placeholder={"johndoe@gmail.com"} 
+            />
 
-        <Input 
-          type = 'email'
-          label={"Email"}
-          state={email}
-          setState={setEmail} 
-          placeholder={"johndoe@gmail.com"} 
-        />
+            <Input 
+              type = 'password'
+              label={"Password"}
+              state={password}
+              setState={setPassword}
+              placeholder={"Example@123"} 
+            />
 
-        <Input 
-          type = 'password'
-          label={"Password"}
-          state={password}
-          setState={setPassword}
-          placeholder={"Example@123"} 
-        />
+            <Button 
+              disabled={loading} 
+              text={loading ? "Loading...": "Login Using Email and Password"} 
+              onClick={loginUsingEmail}
+            />
+            <p className='p-login'>or</p>
+            <Button 
+              text={loading ? "Loading...": "Login Using Google"} 
+              blue={true}
+            />
+            <p 
+              className='p-login' 
+              style={{cursor: 'pointer'}}
+              onClick={()=>setLoginForm(!loginForm)}
+            >
+              Or Don't Have an Account Already? Click Here
+            </p>
+          </form>
+        </div> :  
+        <div className='signup-wrapper'>
+          <h2 className='title'>
+            Sign Up on <span style={{color: "var(--theme)"}}>FinFlow.</span> 
+          </h2>
 
-        <Input 
-          type = 'password'
-          label={"Confirm Password"}
-          state={confirmPassword}
-          setState={setConfirmPassword}
-          placeholder={"Example@123"} 
-        />
+          <form>
+            <Input 
+              type = 'text'
+              label={"Full Name"}
+              state={name}
+              setState={setName}
+              placeholder={"John Doe"} 
+            />
 
-        <Button 
-          disabled={loading} 
-          text={loading ? "Loading...": "Signup Using Email and Password"} 
-          onClick={signupWithEmail}
-        />
-        <p style={{textAlign: 'center', margin: 0}}>or</p>
-        <Button 
-          text={loading ? "Loading...": "Signup Using Google"} 
-          blue={true}
-        />
-      </form>
-    </div>
+            <Input 
+              type = 'email'
+              label={"Email"}
+              state={email}
+              setState={setEmail} 
+              placeholder={"johndoe@gmail.com"} 
+            />
+
+            <Input 
+              type = 'password'
+              label={"Password"}
+              state={password}
+              setState={setPassword}
+              placeholder={"Example@123"} 
+            />
+
+            <Input 
+              type = 'password'
+              label={"Confirm Password"}
+              state={confirmPassword}
+              setState={setConfirmPassword}
+              placeholder={"Example@123"} 
+            />
+
+            <Button 
+              disabled={loading} 
+              text={loading ? "Loading...": "Signup Using Email and Password"} 
+              onClick={signupWithEmail}
+            />
+            <p className='p-login'>or</p>
+            <Button 
+              text={loading ? "Loading...": "Signup Using Google"} 
+              blue={true}
+            />
+            <p 
+              className='p-login'
+              style={{cursor: 'pointer'}}
+              onClick={()=>setLoginForm(!loginForm)}
+            >
+              Or Have an Account Already? Click Here
+            </p>
+          </form>
+        </div>
+      }
+     
+    </>
   )
 }
 
