82fcb3184f2d2f1228497edae4d8aa07ef0c5aff
Rohit Mohite <rohitmohite.in@gmail.com>
2026-06-19 20:16:25 +0530

    transaction added to firestore and fetched them

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Cards from '../components/Cards'
 import { Modal } from 'antd';
 import AddExpenseModal from '../components/Modals/AddExpense';
 import AddIncomeModal from '../components/Modals/AddIncome';
-// import AddExpenseModal from '../components/Modals/addExpense';
-// import AddIncomeModal from '../components/Modals/addIncome';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import moment from 'moment';
+
+
 
 
 
 
 const Dashboard = () => {
+  // const transactions = [
+  //   {
+  //     type: 'income',
+  //     amount: 1200,
+  //     tag: 'salary',
+  //     name: 'income 1',
+  //     date: '2026-06-11'
+  //   },
+  //   {
+  //     type: 'expense',
+  //     amount: 800,
+  //     tag: 'food',
+  //     name: 'expense 1',
+  //     date: '2026-06-19'
+  //   }
+  // ]
+  const [transactions, setTransactions] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [user] = useAuthState(auth)
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
   const showExpenseModal = () => {
@@ -30,27 +54,77 @@ const Dashboard = () => {
   };
 
   const onFinish = (values, type) => {
-    console.log('onFinish', values, type)
+    const newTransaction = {
+      type: type,
+      date: moment(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    };
+    addTransaction(newTransaction);
+  };
+
+  async function addTransaction(transaction) {
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      );
+      console.log("Document written with ID: ", docRef.id);
+     
+      toast.success("Transaction Added!");
+      
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      toast.error("Couldn't add transaction");
+      
+    }
+  }
+
+  useEffect(()=>{
+    if(user){
+      fetchTransactions();
+    }
+  },[user])
+
+  async function fetchTransactions() {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      });
+      setTransactions(transactionsArray);
+      console.log("Transaction array", transactionsArray)
+      toast.success("Transactions Fetched!");
+    }
+    setLoading(false);
   }
 
   return (
     <div>
       <Header/>
-      <Cards
+      {loading ? <p>Loading...</p> : <>
+        <Cards
         showExpenseModal={showExpenseModal}
         showIncomeModal={showIncomeModal} 
-      />
+        />
 
-       <AddExpenseModal
+        <AddExpenseModal
             isExpenseModalVisible={isExpenseModalVisible}
             handleExpenseCancel={handleExpenseCancel}
             onFinish={onFinish}
-          />
+            />
           <AddIncomeModal
             isIncomeModalVisible={isIncomeModalVisible}
             handleIncomeCancel={handleIncomeCancel}
             onFinish={onFinish}
           />
+      </>
+      }
     </div>
   )
 }
