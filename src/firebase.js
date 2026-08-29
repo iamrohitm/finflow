2e8d7bbbf489367226bc27eedd496a5b292c9bd5
Rohit Mohite <rohitmohite.in@gmail.com>
2026-08-29 20:02:57 +0530

    deployment changes are done

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ4pSHqxagfbsbGKszPyxqHMXHAujgVWw",
-  authDomain: "finflow-f1dee.firebaseapp.com",
-  projectId: "finflow-f1dee",
-  storageBucket: "finflow-f1dee.firebasestorage.app",
-  messagingSenderId: "415265252763",
-  appId: "1:415265252763:web:5d952478f7f53af28bad3e",
-  measurementId: "G-T6Y98CXD3Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
